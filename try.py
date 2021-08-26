6605d6937a8abbd724ffb7860c45e45d725d82e8
AmjadMajed <73767435+AmjadMajed@users.noreply.github.com>
2021-08-26 04:46:34 +0300

    Add files via upload

---
+++ try.py
@@ -0,0 +1,15 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+dataset = gpd.read_file(r'C:\GIS Courses\Arcpy\ksa_Regions Polygon.shp')
+
+ax = dataset.plot(cmap = 'OrRd', column = '24_8_2021', legend = True)
+
+
+
+
+ax.set_title('Daily Report for Coronavirus Disease COVID-19 in 24-8-2021')
+
+ax.set_axis_off()
