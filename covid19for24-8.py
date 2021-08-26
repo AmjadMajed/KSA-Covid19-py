251a78f3e0383cba53301c7a054c047f9174f01f
AmjadMajed <73767435+AmjadMajed@users.noreply.github.com>
2021-08-26 04:49:24 +0300

    Update covid19for24-8.py

--- covid19for24-8.py
+++ covid19for24-8.py
@@ -7,9 +7,6 @@ dataset = gpd.read_file(r'C:\GIS Courses\Arcpy\ksa_Regions Polygon.shp')
 
 ax = dataset.plot(cmap = 'OrRd', column = '24_8_2021', legend = True)
 
-
-
-
 ax.set_title('Daily Report for Coronavirus Disease COVID-19 in 24-8-2021')
 
 ax.set_axis_off()
